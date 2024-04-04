7440cc414fa66fe55d00530f74cc720cfb811d0e
Allen Swaidan <allen@Allens-MacBook-Pro.local>
2024-04-04 17:41:16 -0400

    created crop class and cropped more images

---
+++ cropImage.h
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include <iostream>
+#include <utility>
+
+class Crop
+{
+    public:
+        Crop(std::string inputData, std::string outputData, std::string croppingStrategy);
+        bool createOutputDirectory(std::string outputData);
+        void defineROI(int &imageWidth, int &imageHeight);
+        void bottomHalf(std::string inputData, std::string outputData);
+
+    private:
+        cv::Mat image;
+        int x, y, imageWidth, imageHeight, ROIheight;
+
+};
